d1818877847e15459a2501235173a7e0d3395cb5
Helmi <helmi.prasetyo12@gmail.com>
2024-04-09 23:28:31 +0700

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">NextJs 14 We&apos;re Coming</h1>
-      <h2 className="h2-bold">NextJs 14 We&apos;re Coming</h2>
-      <h3 className="h3-bold">NextJs 14 We&apos;re Coming</h3>
-    </div>
-  );
-}
